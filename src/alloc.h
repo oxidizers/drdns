0e95d96a7e992461f205c1611385028895b870bf
Tony Arcieri <bascule@gmail.com>
2017-12-18 11:14:51 -0800

    Nuke original C sources
    Removes all original C code, leaving only the Rust translations.

--- src/alloc.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALLOC_H
-#define ALLOC_H
-
-extern char *alloc(unsigned int n);
-extern void alloc_free(char *x);
-int alloc_re(char **x, unsigned int m,unsigned int n);
-
-#endif
--- src/timeoutwrite.h
+++
@@ -1,6 +0,0 @@
-#ifndef TIMEOUTWRITE_H
-#define TIMEOUTWRITE_H
-
-int timeoutwrite(int t,int fd,char *buf,int len);
-
-#endif
