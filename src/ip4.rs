a4c90362b3441ad53575a84fcf9290a1013e75b2
Tony Arcieri <bascule@gmail.com>
2017-12-22 13:19:41 -0800

    src/ip4.rs: Consolidate from Corroded `ip4_*.rs`
    Consolidates all Corrode-translated source code for `ip4_*.rs`
    functionality into a common `ip4` module.
    
    Code is otherwise unchanged from the original Corrode output.

--- src/ip4.rs
+++ src/ip4.rs
@@ -1,6 +1,58 @@
 use ulong;
 
 #[no_mangle]
+pub unsafe extern "C" fn ip4_fmt(mut s: *mut u8, mut ip: *const u8) -> u32 {
+    let mut len: u32;
+    let mut i: u32;
+    len = 0u32;
+    i = ulong::fmt(s, *ip.offset(0isize) as (usize));
+    len = len.wrapping_add(i);
+    if !s.is_null() {
+        s = s.offset(i as (isize));
+    }
+    if !s.is_null() {
+        *{
+            let _old = s;
+            s = s.offset(1isize);
+            _old
+        } = b'.';
+    }
+    len = len.wrapping_add(1u32);
+    i = ulong::fmt(s, *ip.offset(1isize) as (usize));
+    len = len.wrapping_add(i);
+    if !s.is_null() {
+        s = s.offset(i as (isize));
+    }
+    if !s.is_null() {
+        *{
+            let _old = s;
+            s = s.offset(1isize);
+            _old
+        } = b'.';
+    }
+    len = len.wrapping_add(1u32);
+    i = ulong::fmt(s, *ip.offset(2isize) as (usize));
+    len = len.wrapping_add(i);
+    if !s.is_null() {
+        s = s.offset(i as (isize));
+    }
+    if !s.is_null() {
+        *{
+            let _old = s;
+            s = s.offset(1isize);
+            _old
+        } = b'.';
+    }
+    len = len.wrapping_add(1u32);
+    i = ulong::fmt(s, *ip.offset(3isize) as (usize));
+    len = len.wrapping_add(i);
+    if !s.is_null() {
+        s = s.offset(i as (isize));
+    }
+    len
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn ip4_scan(mut s: *const u8, mut ip: *mut u8) -> u32 {
     let mut i: u32;
     let mut len: u32;
